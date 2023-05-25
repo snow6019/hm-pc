763fe9f4748e89b37f9f8ced419cafb03f64fcff
wsn <123@qq.com>
2023-05-25 10:51:21 +0800

    update

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,15 +2,15 @@
   <div class="login-page">
     <el-card class="el-card">
       <template #header>黑马面经运营后台</template>
-      <el-form>
-        <el-form-item label="用户名：">
-          <el-input placeholder="请输入用户名" />
+      <el-form ref="form" :model="form" :rules="rules">
+        <el-form-item label="用户名：" prop="username">
+          <el-input v-model="form.username" placeholder="请输入用户名" />
         </el-form-item>
-        <el-form-item label="密码：">
-          <el-input placeholder="请输入密码：" />
+        <el-form-item label="密码：" prop="password">
+          <el-input type="password" v-model="form.password" placeholder="请输入密码：" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button>重置</el-button>
         </el-form-item>
       </el-form>
@@ -20,7 +20,35 @@
 
 <script>
 export default {
-  name: 'login-page'
+  name: 'login-page',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: ['blur', 'change'] },
+          { min: 5, max: 11, message: '长度在 5 到 11 个字符', trigger: ['blur', 'change'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['blur', 'change'] },
+          { pattern: /^\w{5,11}$/, message: '请输入 5 到 10 位的密码', trigger: ['blur', 'change'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          const res = await this.$axios({ url: '/auth/login', methods: 'POST', data: this.form })
+          console.log(res)
+        }
+      })
+    }
+  }
 }
 </script>
 
